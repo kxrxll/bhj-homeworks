c3caf28f6663273a28a44eff52a8178e2468d9e0
kxrxll <kovalyovkirill@gmail.com>
2021-06-23 09:54:42 +0300

    done

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -2,13 +2,16 @@
 const highlighted = Array.from(document.querySelectorAll(".has-tooltip"));
 //Handlers
 function showTooltip(evt) {
+  evt.preventDefault();
+  if(evt.target.nextElementSibling && evt.target.nextElementSibling.classList. contains('tooltip_active')){
+    evt.target.nextElementSibling.classList.remove('tooltip_active');
+    return;
+  }
   const activeTooltips = Array.from(document.querySelectorAll(".tooltip"));
   activeTooltips.forEach((item) => item.remove());
-  evt.preventDefault();
   const tooltipText = evt.target.title;
   const posX = evt.target.offsetLeft;
   const posY = evt.target.offsetTop - evt.target.height;
-  console.log();
   evt.target.insertAdjacentHTML(
     "afterEnd",
     `<div class="tooltip tooltip_active" style="left: ${posX}px; top: ${posY}px; position: absolute" data-position="bottom">
